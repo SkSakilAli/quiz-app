2a7d86689239dc2117a3036439e0cbaf4ee5e17e
Sk Sakil Ali <187862446+SkSakilAli@users.noreply.github.com>
2025-07-28 23:26:07 +0530

    Add JavaScript Complete

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const answers = {
      0: "Who Created JavaScript?",
      1: "When was JavaScript Created?",
      2: "What will be the value of '1' == 1",
-     3: "What will be the value of '1'+'1' ",
-     5: "Which one from the below is a primitive data type",
+     3: "What will be the value of 1+1 ",
+     4: "Which one from the below is a primitive data type",
      },
-     optionOne: ["1990", "1990"],
-     optionTwo: ["x", "Y"],
-     correctAnswer: [1,2],
+     optionOne: ["Brenden Eich", "1990", "true", "2", "string"],
+     optionTwo: ["Tim-Berners Lee", "1995", "false", "11", "array"],
+     correctAnswer: [1,2,2,1,2,,1],
      }
 QUES.textContent = answers.question[count];
 OPONE.textContent = answers.optionOne[count];
@@ -26,16 +26,18 @@ SCR.textContent = score;
 
 //Checking Correct Answers
 function check(option){
-	if option == correctAnswe[count]{
-            score++
+	if(option == answers.correctAnswer[count]){
+            score++;
+	    SCR.textContent = score;
 	}
 	count++
 
-if(option!=0){
-
- 
-}
-else{ console.error("Option equates to zero" );
 }
 
+
+function next(){
+
+QUES.textContent = answers.question[count];
+OPONE.textContent = answers.optionOne[count];
+OPTWO.textContent = answers.optionTwo[count];
 }
